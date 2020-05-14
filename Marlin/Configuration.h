6ba63dc7091f9d0ed0695f58b32ee22e4796b5d5
Siddhartha Dhiman <sid.kill3r@gmail.com>
2020-05-14 00:52:52 -0400

    Update BL Touch Z offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -961,7 +961,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -52, -20, 4 }
+#define NOZZLE_TO_PROBE_OFFSET { -52, -20, -2.1 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
